52fc44edd411b3db42b8f84bdcb6252a61837ea0
uamadasun <uchennaamadasun@gmail.com>
2022-10-18 13:10:40 -0700

    completed liking likes assignment

---
+++ Stack01_WebFundamentals/Assignment17_LikingLikes/part2/script.js
@@ -0,0 +1,26 @@
+// BUTTON1
+var likes1 = 9;
+var targetElement1 = document.querySelector("#button1likes")
+
+function click1(element) {
+    likes1 += 1
+    targetElement1.innerText = likes1 + " like(s)"
+}
+
+// BUTTON2
+var likes2 = 12;
+var targetElement2 = document.querySelector("#button2likes")
+
+function click2(element) {
+    likes2 += 1
+    targetElement2.innerText = likes2 + " like(s)"
+}
+
+//BUTTON3
+var likes3 = 9;
+var targetElement3 = document.querySelector("#button3likes")
+
+function click3(element) {
+    likes3 += 1
+    targetElement3.innerText = likes3 + " like(s)"
+}
