5a045f5bc1283caa264899277bbfee088d9536b1
uamadasun <uchennaamadasun@gmail.com>
2022-12-03 04:47:02 -0800

    updated daikichi assignment

--- java/springProjects/Daikichi/src/main/java/com/uchennaamadasun/daikichi/MainController.java
+++ java/springProjects/Daikichi/src/main/java/com/uchennaamadasun/daikichi/MainController.java
@@ -1,5 +1,6 @@
 package com.uchennaamadasun.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,20 @@ public class MainController {
 	@RequestMapping("/tomorrow")
 	public String tomorrowMessage() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String showCity(@PathVariable("city") String city) {
+		city = city.substring(0, 1).toUpperCase() + city.substring(1);
+		return String.format("Congratulations! You will soon travel to %s!", city);
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String newLotto(@PathVariable("number") Integer number) {
+		if(number%2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 	
 
