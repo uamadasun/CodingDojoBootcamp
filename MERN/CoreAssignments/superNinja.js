f5bc22db7cb955e476de60be680673d23f1fefaf
uamadasun <uchennaamadasun@gmail.com>
2023-01-08 08:11:33 -0800

    ninja and super ninja assignments added

---
+++ MERN/CoreAssignments/superNinja.js
@@ -0,0 +1,48 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 0;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed =10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        this.drinkSake();
+        console.log(`"I'm making money moves"`);
+        this.wisdom += 20;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
---
+++ MERN/PracticeAssignments/ninja.js
@@ -0,0 +1,27 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 0;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+
