e382e00bfd1a61ea2045474c7b45ff5d271d9a2e
uamadasun <uchennaamadasun@gmail.com>
2023-01-10 11:52:25 -0800

    new firstjsx assignment

---
+++ MERN/CoreAssignments/firstJSX/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App font-link">
+
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
