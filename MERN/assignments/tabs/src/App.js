750fc538a449d7ed700c5ee59fc84d5edb2fec35
uamadasun <uchennaamadasun@gmail.com>
2023-01-14 14:10:02 -0800

    tabs assignment updated.

---
+++ MERN/assignments/tabs/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Tab from "./components/Tab";
+import React, { useState } from 'react';
+
+
+function App() {
+
+
+  const tabObj = [{name:"Tab 1", content: "Tab 1 Content"}, {name:"Tab 2", content: "Tab 2 Content"}, {name:"Tab 3", content: "Tab 3 Content"}]
+
+  // const tab1 = {name:"Tab 1", content: "Tab 1 Content"};
+  // const tab2 = {name:"Tab 2", content: "Tab 2 Content"};
+  // const tab3 = {name:"Tab 3", content: "Tab 3 Content"};
+  // const [currentContent, setCurrentContent] = useState("");
+  
+  const [allTabs, setAllTabs] = useState(tabObj);
+  const [currentTab, setCurrentTab] = useState(0);
+  // const getContent = (content) => {
+  //   console.log("content from app.js" + content)
+  //   setCurrentContent(content);
+  // }
+  return (
+
+    <div className="App">
+      {/* <Tab arr={[tab1, tab2, tab3]} getContent = { getContent }/> */}
+      <Tab allTabs={allTabs} />
+      
+
+      
+    </div>
+
+  );
+}
+
+export default App;
---
+++ MERN/assignments/tabs/src/components/Tab.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const Tab = (props) => {
+    // const [currentTab, setCurrentTab] = useState("");
+    // console.log("This is all tabs" + props.allTabs);
+    const [content, setContent] = useState("");
+
+
+    const onClickHandler = (e, item) => {
+        // console.log(item.content);
+        // setCurrentTab(item)
+        console.log(item);
+        setContent(item.content);
+        // props.getContent = item.content;
+
+    }
+
+    return(
+        <div className = "container mt-8">
+
+            <div className = "d-inline-flex align-content-middle">
+                {props.allTabs.map((item,i) => 
+                <div key={i}> 
+                <button className="btn btn-success m-3" name={item} onClick={(e) => onClickHandler(e, item)}> {item.name} </button> </div>
+                )}
+            </div>
+            <div className = "card">
+                {content}
+            </div>
+            
+        </div>
+    )
+
+//     return (
+        
+
+//         props.arr.map( (item, i) => { 
+//         return (
+//             <div>
+//             <div key={i} className = "d-inline-flex align-content-middle">
+//                 <button name={item} value={item.name} onClick={(e)=>onClickHandler(e, item)}> { item.name } </button>
+//             </div>
+//             {/* <div>this is where the box should go </div> */}
+            
+            
+//             </div>
+        
+//     )})
+//     // <div>this is where the box should go </div>
+    
+// );
+            
+
+    
+}
+
+export default Tab;
