66ecd3b818fa037a99da9cc5567c2740c2922e34
uamadasun <uchennaamadasun@gmail.com>
2023-01-17 12:29:25 -0800

    complete pokemon api axios assignment

--- MERN/assignments/14pokemonaxios/src/App.js
+++ MERN/assignments/14pokemonaxios/src/App.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+// import GetPokemon from './components/GetPokemon';
 
 //allPokemon doesn't show up as multiple pokemon objects, just as one object that is an array of objects
 //says we should get 807 of them?? only shows up 20
@@ -10,31 +11,24 @@ function App() {
   const [allPokemon, setAllPokemon] = useState([]);
 
   const GetPokemon = () => {
-    const [responseData, setResponseData] = useState(null);
-    useEffect(()=>{
-        axios.get('https://pokeapi.co/')
-            .then(response=>{setResponseData(response.data)})
-    }, []); 
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=808')
+        .then(response=>{setAllPokemon(response.data.results)})
+    }; 
 
-    setAllPokemon(responseData);
-    console.log("all pokemon: " + allPokemon);
-  }
-  
+
+
   return (
-
-    
-
     <div className="App container mt-5">
       <button className='btn btn-warning' onClick={GetPokemon}> Get Pokemon</button>
-      {allPokemon.map((pokemon, idx) =>
-      <div key={idx}> 
-        <ul className="list-group mt-3">
-            <li className="list-group-item row g-1">
-              {pokemon.name}
-            </li>
-        </ul>
-      </div>
-      )}
+            {allPokemon.map((pokemon, idx) =>
+            <div key={idx}> 
+                    <ul className="list-group mt-3">
+                        <li className="list-group-item row g-1">
+                            {pokemon.name}
+                        </li>
+                    </ul>
+            </div>
+            )}
       
     </div>
   );
